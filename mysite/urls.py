f9de9845b55e69bec7b925ef37dca5c00e21739e
Ignacio <antonionacho@live.com>
2021-01-25 18:27:28 -0300

    se creo loggin y registro

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),#url panerl de administracion
     path('', include('turismo.urls')),#colocando paguina inical
-   
+    path('accounts/', include('django.contrib.auth.urls')), 
 ]
--- turismo/form.py
+++ turismo/form.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Contacto, Regiones
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 class ContactoForm(forms.ModelForm):
 
     class Meta:
@@ -11,3 +13,6 @@ class RegionForm(forms.ModelForm):
     class Meta:
         model = Regiones
         fields = '__all__'
+    
+class CustomUserCreationForm(UserCreationForm):
+    pass
